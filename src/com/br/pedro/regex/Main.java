e40dfde7cc0741072729a5e48f504c5cb4303a8e
Pedro Daniel Jardim <pedro.jardim@ufv.br>
2021-02-13 20:23:56 -0300

    Main Class

---
+++ src/com/br/pedro/regex/Main.java
@@ -0,0 +1,19 @@
+package com.br.pedro.regex;
+
+import com.br.pedro.regex.exercises.Exercise1;
+import com.br.pedro.regex.exercises.Exercise2;
+import com.br.pedro.regex.exercises.Exercise3;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Exercise1 exercise1 = new Exercise1();
+//        exercise1.exercise();
+
+//        Exercise2 exercise2 = new Exercise2();
+//        exercise2.executeRegex();
+
+        Exercise3 exercise3 = new Exercise3();
+        exercise3.executeRegex();
+    }
+}
